2e8b615e95a7309ba712da97aae73104b8ac16d5
csordasattila <attila.csordas@gravityrd.com>
2020-07-06 17:24:58 +0200

    Add test components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+
+import "./App.css";
+
+import Courses from './pages/Courses'
+import Course from './pages/Course'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>header navigation</div>
+      <Switch>
+        <Route path="/courses">
+          <Courses />
+        </Route>
+        <Route path="/course_one">
+          <Course pageTitle="course one"/>
+        </Route>
+        <Route path="/course_two">
+          <div>courses two</div>
+        </Route>
+        <Route path="/">
+          <div>
+            main
+          </div>
+        </Route>
+      </Switch>
+      <div>footer</div>
     </div>
   );
 }
