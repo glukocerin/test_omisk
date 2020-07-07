e3eb8d30ea3fc5ecf88ab18c041b8490574574a5
TAJ <bestrapboy@gmail.com>
2020-07-07 22:24:02 +0200

    Add navigation, move related code into

--- src/App.js
+++ src/App.js
@@ -3,31 +3,21 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect
 } from "react-router-dom";
 
 import "./assets/css/colors.css";
 import "./App.css";
-import { ReactComponent as Logo } from "./assets/img/logo.svg";
 
 import Courses from "./pages/Courses";
 import Course from "./pages/Course";
 import Landing from "./pages/Landing";
+import Navigation from "./pages/Navigation";
 
 function App() {
   return (
     <div className="App">
-      <div className="header">
-        <Logo className="logo" />
-        <Link to="/courses">
-          <Courses />
-        </Link>
-        <Link to="/course_one">
-          <Course pageTitle="course one" />
-        </Link>
-        <Link to="/course_two">courses two link</Link>
-      </div>
+      <Navigation />
       <Switch>
         <Route path="/courses">
           <Courses />
---
+++ src/pages/Navigation.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import {
+    Link
+  } from "react-router-dom";
+
+import '../assets/css/navigation.css';
+import { ReactComponent as Logo } from "../assets/img/logo.svg";
+
+import Courses from "../pages/Courses";
+import Course from "../pages/Course";
+
+export default function Navigation() {
+    return(
+        <div className="header">
+        <Logo className="logo" />
+        <Link to="/courses">
+          <Courses />
+        </Link>
+        <Link to="/course_one">
+          <Course pageTitle="course one" />
+        </Link>
+        <Link to="/course_two">courses two link</Link>
+      </div>
+    )
+}
