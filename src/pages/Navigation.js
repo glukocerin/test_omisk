8f8f98b7c945888f476460e968d0e40da31349a0
csati <ati.csordas@gmail.com>
2020-07-07 23:46:18 +0200

    Fix navigation

--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -1,28 +1,22 @@
-import React from 'react'
+import React from "react";
 
-import {
-    Link
-  } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import '../assets/css/navigation.css';
+import "../assets/css/navigation.css";
 import { ReactComponent as Logo } from "../assets/img/logo.svg";
 
 import Courses from "../pages/Courses";
 import Course from "../pages/Course";
 
 export default function Navigation() {
-    return(
-        <div className="header">
-        <Logo className="logo" />
-        <div className="links">
-            <Link to="/courses">
-            <Courses />
-            </Link>
-            <Link to="/course_one">
-            <Course pageTitle="course one" />
-            </Link>
-            <Link to="/course_two">courses two link</Link>
-        </div>
+  return (
+    <div className="header">
+      <Logo className="logo" />
+      <div className="links">
+        <Link to="/courses">Courses</Link>
+        <Link to="/course_one">Course one</Link>
+        <Link to="/course_two">courses two link</Link>
       </div>
-    )
+    </div>
+  );
 }
