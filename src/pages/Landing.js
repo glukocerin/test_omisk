8dff8f683baaf251abeb77adcea0dd57f5ec0c5c
TAJ <bestrapboy@gmail.com>
2020-07-22 20:30:52 +0200

    reorder landing pages for mobile view

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -34,9 +34,11 @@ export default function Landing() {
                     <label className="text extra-bold size-54">»</label>
                 </div>
             </div>
+            <div className={`right one ${ activeDot === 1 ? "active": "" }`}></div>
             <div className={`left two ${ activeDot === 2 ? "active": "" }`}>
                 <label className="text extra-bold size-70">1990 óta tudatos testképzés, kreativitás, közösség teremtés</label>
             </div>
+            <div className={`right two ${ activeDot === 2 ? "active": "" }`}></div>
             <div className={`left three ${ activeDot === 3 ? "active": "" }`}>
                 <div className="content-block">
                     <label className="title extra-bold size-54">Kik vagyunk?</label>
@@ -50,6 +52,7 @@ export default function Landing() {
                     </div>
                 </div>
             </div>
+            <div className={`right three ${ activeDot === 3 ? "active": "" }`}></div>
             <div className={`left four ${ activeDot === 4 ? "active": "" }`}>
                 <div className="content-block">
                     <label className="title extra-bold size-54">Helyszíneink</label>
@@ -193,6 +196,9 @@ export default function Landing() {
                     </div>
                 </div>
             </div>
+            <div className={`right four ${ activeDot === 4 ? "active": "" }`}>
+                <LeafletMap props={activePlace}/>
+            </div>            
             <div className={`left five ${ activeDot === 5 ? "active": "" }`}>
                 <div className="content-block">
                     <label className="title extra-bold size-54">Milyen óráink vannak?</label>
@@ -222,12 +228,23 @@ export default function Landing() {
                    </div>
                 </div>
             </div>
+            <div className={`right five ${ activeDot === 5 ? "active": "" }`}></div>
             <div className={`left six ${ activeDot === 6 ? "active": "" }`}>
                 <div className="content-block">
                     <label className="title extra-bold size-54">Mit csinálunk az órákon?</label>
                     <label className="article size-20">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</label>
                     <label className="article size-20">At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</label>
                     <label className="article size-20">At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</label>
+                </div>
+            </div>
+            <div className={`right six ${ activeDot === 6 ? "active": "" }`}>
+                <div className="content-block">
+                    <iframe title="video" width="416" height="234" src={youtubeLinks[activeLink]} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <div className="link-block">
+                        <label onClick={() => setActiveLink(0)} className={`link size-15 ${ activeLink === 0 && 'active' }`}>Művészi torna</label>
+                        <label onClick={() => setActiveLink(1)} className={`link size-15 ${ activeLink === 1 && 'active' }`}>Mozdulatművészet</label>
+                        <label onClick={() => setActiveLink(2)} className={`link size-15 ${ activeLink === 2 && 'active' }`}>Modern tánc</label>
+                    </div>
                 </div>
             </div>
             <div className={`left seven ${ activeDot === 7 ? "active": "" }`}>
@@ -259,6 +276,7 @@ export default function Landing() {
                    </div>
                 </div>
             </div>
+            <div className={`right seven ${ activeDot === 7 ? "active": "" }`}></div>
             <div className={`left eight ${ activeDot === 8 ? "active": "" }`}>
                 <div className="content-block">
                     <label className="title extra-bold size-54">Ami fontos nekünk</label>
@@ -282,6 +300,7 @@ export default function Landing() {
                     <label className="article size-20">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</label>
                 </div>
             </div>
+            <div className={`right eight ${ activeDot === 8 ? "active": "" }`}></div>
             <div className="dots">
                 <div onClick={() => setActiveDot(1)} className={`dot ${ activeDot === 1 ? "active":""}`}></div>
                 <div onClick={() => setActiveDot(2)} className={`dot ${ activeDot === 2 ? "active":""}`}></div>
@@ -294,25 +313,6 @@ export default function Landing() {
                 <div onClick={() => setActiveDot(7)} className={`dot ${ activeDot === 7 ? "active":""}`}></div>
                 <div onClick={() => setActiveDot(8)} className={`dot ${ activeDot === 8 ? "active":""}`}></div>
             </div>
-            <div className={`right one ${ activeDot === 1 ? "active": "" }`}></div>
-            <div className={`right two ${ activeDot === 2 ? "active": "" }`}></div>
-            <div className={`right three ${ activeDot === 3 ? "active": "" }`}></div>
-            <div className={`right four ${ activeDot === 4 ? "active": "" }`}>
-                <LeafletMap props={activePlace}/>
-            </div>
-            <div className={`right five ${ activeDot === 5 ? "active": "" }`}></div>
-            <div className={`right six ${ activeDot === 6 ? "active": "" }`}>
-                <div className="content-block">
-                    <iframe title="video" width="416" height="234" src={youtubeLinks[activeLink]} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                    <div className="link-block">
-                        <label onClick={() => setActiveLink(0)} className={`link size-15 ${ activeLink === 0 && 'active' }`}>Művészi torna</label>
-                        <label onClick={() => setActiveLink(1)} className={`link size-15 ${ activeLink === 1 && 'active' }`}>Mozdulatművészet</label>
-                        <label onClick={() => setActiveLink(2)} className={`link size-15 ${ activeLink === 2 && 'active' }`}>Modern tánc</label>
-                    </div>
-                </div>
-            </div>
-            <div className={`right seven ${ activeDot === 7 ? "active": "" }`}></div>
-            <div className={`right eight ${ activeDot === 8 ? "active": "" }`}></div>
         </div>
     )
 }
