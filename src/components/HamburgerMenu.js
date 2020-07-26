f8b18b04de53ed17d4e2fd4634bbe9747f9e2e49
TAJ <bestrapboy@gmail.com>
2020-07-27 00:10:57 +0200

    add underlined style for hamburger menu link if it's state is the current state

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../assets/css/hamburger.menu.css";
 import { ReactComponent as Logo } from "../assets/img/logo.svg";
 import { ReactComponent as Facebook } from "../assets/img/facebook.svg";
@@ -7,7 +7,8 @@ import { ReactComponent as Instagram } from "../assets/img/insta.svg";
 import { ReactComponent as Youtube } from "../assets/img/youtube.svg";
 
 export default function HamburgerMenu() {
-  const [toggled, toggle] = useState(false);
+  const [toggled, toggle] = useState(false),
+        location = useLocation().pathname;
 
   return (
     <div className={`hamburger-menu ${ toggled && "toggled"}`}>
@@ -22,14 +23,14 @@ export default function HamburgerMenu() {
         </div>
       </div>
       <div className="menu-list">
-        <Link className="link size-36 extra-bold" onClick={() => toggle(!toggled)} to="/">Kezdő</Link>
-        <Link className="link size-36 extra-bold" onClick={() => toggle(!toggled)} to="/courses">Kurzusaink</Link>
-        <Link className="link size-36 extra-bold" onClick={() => toggle(!toggled)} to="/programs">Programjaink</Link>
-        <Link className="link size-36 extra-bold" onClick={() => toggle(!toggled)} to="/teachers">Tanáraink</Link>
-        <Link className="link size-36 extra-bold" onClick={() => toggle(!toggled)} to="/actual">Aktuális</Link>
-        <Link className="link size-36 extra-bold" onClick={() => toggle(!toggled)} to="/operation">Működésünk</Link>
-        <Link className="link size-36 extra-bold" onClick={() => toggle(!toggled)} to="/enrollment">Beiratkozás</Link>
-        <Link className="link size-36 extra-bold" onClick={() => toggle(!toggled)} to="/contact">Kapcsolat</Link>
+        <Link className={`link size-36 extra-bold ${ location === "/" && "active" }`} onClick={() => toggle(!toggled)} to="/">Kezdő</Link>
+        <Link className={`link size-36 extra-bold ${ location === "/courses" && "active" }`} onClick={() => toggle(!toggled)} to="/courses">Kurzusaink</Link>
+        <Link className={`link size-36 extra-bold ${ location === "/programs" && "active" }`} onClick={() => toggle(!toggled)} to="/programs">Programjaink</Link>
+        <Link className={`link size-36 extra-bold ${ location === "/teachers" && "active" }`} onClick={() => toggle(!toggled)} to="/teachers">Tanáraink</Link>
+        <Link className={`link size-36 extra-bold ${ location === "/actual" && "active" }`} onClick={() => toggle(!toggled)} to="/actual">Aktuális</Link>
+        <Link className={`link size-36 extra-bold ${ location === "/operation" && "active" }`} onClick={() => toggle(!toggled)} to="/operation">Működésünk</Link>
+        <Link className={`link size-36 extra-bold ${ location === "/enrollment" && "active" }`} onClick={() => toggle(!toggled)} to="/enrollment">Beiratkozás</Link>
+        <Link className={`link size-36 extra-bold ${ location === "/contact" && "active" }`} onClick={() => toggle(!toggled)} to="/contact">Kapcsolat</Link>
         <div className="sm-icons">
             <div className="icon">
                 <Instagram/>
