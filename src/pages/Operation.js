0b7084290cba2f9fcc5e44d0bfba4c8a6f087253
TAJ <bestrapboy@gmail.com>
2020-07-31 11:47:55 +0200

    move operations to pagesConfig

--- src/pages/Operation.js
+++ src/pages/Operation.js
@@ -4,6 +4,23 @@ import '../assets/css/operation.css';
 import { ReactComponent as PlusSign } from "../assets/img/icons/plus.svg";
 import { ReactComponent as MinusSign } from "../assets/img/icons/minus.svg";
 
+import pagesConfig from "../assets/pagesConfig";
+
+const { operations, ...rest } = pagesConfig;
+
+const OperationElements = function(props) {
+  return operations.map((operation, index) => (
+    <div className={`info  ${ operation.role === 'primary' && 'primary' } ${ props.menuItem === index+1 && 'active' }`} onClick={() => props.setActive(index+1) } key={index}>
+        <PlusSign className="plus-sign"/>
+        <MinusSign className="minus-sign"/>
+        <label className="info-label size-32 extra-bold">{operation.title}</label>
+        <div className="info-informations">
+            {operation.description && operation.description}
+        </div>
+    </div>
+  ));
+};
+
 export default function Operations() {
   const [ActiveMenuItem, setActiveMenuItem] = useState(null);
 
@@ -13,79 +30,8 @@ export default function Operations() {
       <label className="title size-90 extra-bold">Működésünk</label>
       <div className="operation-content">
         <div className="drop-down-list">
-            <div className={`info primary ${ ActiveMenuItem === 1 && 'active' }`} onClick={() => setActiveMenuItem(1) }>
-                <PlusSign className="plus-sign"/>
-                <MinusSign className="minus-sign"/>
-                <label className="info-label size-32 extra-bold">Általános tájékoztató a jelentkezés menetéről és az Iskola rendjéről</label>
-                <div className="info-informations">
-
-                </div>
-            </div>
-            <div className={`info ${ ActiveMenuItem === 2 && 'active' }`} onClick={() => setActiveMenuItem(2) }>
-                <PlusSign className="plus-sign"/>
-                <MinusSign className="minus-sign"/>                            
-                <label className="info-label size-24 extra-bold">csatlakozás év közben</label>
-                <div className="info-informations">
-
-                </div>
-            </div>
-            <div className={`info ${ ActiveMenuItem === 3 && 'active' }`} onClick={() => setActiveMenuItem(3) }>
-                <PlusSign className="plus-sign"/>
-                <MinusSign className="minus-sign"/>                            
-                <label className="info-label size-24 extra-bold">tandíjak, befizetés</label>
-                <div className="info-informations">
-
-                </div>
-            </div>
-            <div className={`info ${ ActiveMenuItem === 4 && 'active' }`} onClick={() => setActiveMenuItem(4) }>
-                <PlusSign className="plus-sign"/>
-                <MinusSign className="minus-sign"/>                            
-                <label className="info-label size-24 extra-bold">engedmények</label>
-                <div className="info-informations">
-
-                </div>
-            </div>
-            <div className={`info ${ ActiveMenuItem === 5 && 'active' }`} onClick={() => setActiveMenuItem(5) }>
-                <PlusSign className="plus-sign"/>
-                <MinusSign className="minus-sign"/>                            
-                <label className="info-label size-24 extra-bold">hiányzás</label>
-                <div className="info-informations">
-
-                </div>
-            </div>
-            <div className={`info ${ ActiveMenuItem === 6 && 'active' }`} onClick={() => setActiveMenuItem(6) }>
-                <PlusSign className="plus-sign"/>
-                <MinusSign className="minus-sign"/>                            
-                <label className="info-label size-24 extra-bold">öltözet</label>
-                <div className="info-informations">
-
-                </div>
-            </div>
-            <div className={`info ${ ActiveMenuItem === 7 && 'active' }`} onClick={() => setActiveMenuItem(7) }>
-                <PlusSign className="plus-sign"/>
-                <MinusSign className="minus-sign"/>                            
-                <label className="info-label size-24 extra-bold">egyéb kiadások</label>
-                <div className="info-informations">
-
-                </div>
-            </div>
-            <div className={`info primary ${ ActiveMenuItem === 8 && 'active' }`} onClick={() => setActiveMenuItem(8) }>
-                <PlusSign className="plus-sign"/>
-                <MinusSign className="minus-sign"/>                            
-                <label className="info-label size-32 extra-bold">Tanév rendje, 2020</label>
-                <div className="info-informations">
-
-                </div>
-            </div>
-            <div className={`info primary ${ ActiveMenuItem === 8 && 'active' }`} onClick={() => setActiveMenuItem(8) }>
-                <PlusSign className="plus-sign"/>
-                <MinusSign className="minus-sign"/>                            
-                <label className="info-label size-32 extra-bold">Tájékoztató kezdő ovis növendékeink szüleinek</label>
-                <div className="info-informations">
-
-                </div>
-            </div>
-          </div>
+            <OperationElements menuItem={ActiveMenuItem} setActive={setActiveMenuItem} />
+        </div>
       </div>
     </div>
   );
