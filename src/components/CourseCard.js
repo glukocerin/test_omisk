398ed33ce66681026b450e115265e866964b37f9
csati <ati.csordas@gmail.com>
2020-08-11 23:35:52 +0200

    Courses refact

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -4,31 +4,16 @@ import style from "../assets/css/courseCard.module.css";
 
 export default function Card(props) {
   return (
-    <div className={`${style.wrapper} ${style[props.details.backgroundColor]}`}>
+    <Link to={`/courses/${props.details.link}`} className={`${style.wrapper}`}>
+      <labe className={`${style.title} size-28 extra-bold`}>
+        {props.details.title}
+      </labe>
       <div className={style["image-wrapper"]}>
         <img
           src={require(`../assets/img/courses/${props.details.image}.jpg`)}
           alt=""
         />
       </div>
-      <div className={style["content"]}>
-        <div className={style["text"]}>
-          <span className={style["title"]}>{props.details.title}</span>
-          <span className={style["details"]}>{props.details.age}</span>
-          <span className={style["details"]}>{props.details.districts}</span>
-          <span className={style["details"]}>{props.details.bottomText}</span>
-        </div>
-      </div>
-      <div className={style["buttons"]}>
-        <div className={style["sign-up"]}>
-          <span>Jelentkezem</span>
-        </div>
-        <Link className={style["link"]} to={`/courses/${props.details.link}`}>
-          <div className={style["more-details"]}>
-            <span className={`${style[props.details.dotColor]}`}>•••</span>
-          </div>
-        </Link>
-      </div>
-    </div>
+    </Link>
   );
 }
