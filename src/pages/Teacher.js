fd1d4f4502b1132c5c2ced68f1e465dee973df1a
csati <ati.csordas@gmail.com>
2020-08-12 01:02:32 +0200

    update teachers teachers page

--- src/pages/Teacher.js
+++ src/pages/Teacher.js
@@ -1,11 +1,13 @@
 import React from "react";
-import '../assets/css/teacher.css';
+import "../assets/css/teacher.css";
 
 import { Link, useLocation } from "react-router-dom";
 
 const ListItems = function(props) {
   return props.list.map((item, index) => (
-    <li className="list-item size-15" key={index}>{item}</li>
+    <li className="list-item size-15" key={index}>
+      {item}
+    </li>
   ));
 };
 
@@ -16,26 +18,40 @@ export default function Teacher() {
     <div className="teacher">
       <div className="teacher-header"></div>
       <label className="state-location size-13">
-        <Link to="/teachers">Tanáraink</Link> > { teacher.name }
+        <Link to="/teachers">Tanáraink</Link> > {teacher.name}
       </label>
-      <label className="title size-90 extra-bold">{ teacher.name }</label>
-      <img className="profile-picture" src={ require(`../assets/img/teachers/${teacher.picture}`)} alt="" />
+      <label className="title size-90 extra-bold">{teacher.name}</label>
+      <img
+        className="profile-picture"
+        src={require(`../assets/img/teachers/${teacher.picture}`)}
+        alt=""
+      />
       <div className="columns">
         <div className="column-left">
-          <label className="name size-36 extra-bold">{ teacher.name }</label>
-          <label className="desc size-15 thin">{ teacher.desc }</label>
-          <label className="details size-20">{ teacher.details }</label>
+          <label className="name size-36 extra-bold">{teacher.name}</label>
+          <label className="desc size-15 thin">{teacher.desc}</label>
+          <label className="details size-20">{teacher.details}</label>
         </div>
         <div className="column-right">
           <label className="text size-24 bold">Végzettség:</label>
           <ul className="list">
             <ListItems list={teacher.education} />
           </ul>
-          <label className={`text size-24 bold ${teacher.achievements.length === 0 && 'hidden'}`}>Kitüntetések, díjak:</label>
+          <label
+            className={`text size-24 bold ${teacher.achievements.length === 0 &&
+              "hidden"}`}
+          >
+            Kitüntetések, díjak:
+          </label>
           <ul className="list">
             <ListItems list={teacher.achievements} />
           </ul>
-          <label className={`text size-24 bold ${teacher.courses.length === 0 && 'hidden'}`}>Óratípusok:</label>
+          <label
+            className={`text size-24 bold ${teacher.courses.length === 0 &&
+              "hidden"}`}
+          >
+            Óratípusok:
+          </label>
           <ul className="list">
             <ListItems list={teacher.courses} />
           </ul>
