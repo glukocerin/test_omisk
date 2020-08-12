cdde644b44323faa3f9e3612d03fe28de2e46edf
TAJ <bestrapboy@gmail.com>
2020-08-12 23:20:39 +0200

    add footer for mobile view

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ import Achievement from "./pages/Achievement";
 import Landing from "./pages/Landing";
 
 import Footer from "./components/Footer";
+import FooterMobile from "./components/FooterMobile";
 import Navigation from "./pages/Navigation";
 import Programs from "./pages/Programs";
 import ProgramDetail from "./pages/ProgramDetail";
@@ -54,111 +55,133 @@ function App() {
           <Navigation BackgroundColor="green-1" />
           <Courses />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route path="/teachers">
           <Navigation BackgroundColor="pink" isHeaderFixed="true" />
           <Teachers />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route path="/teacher">
           <Navigation BackgroundColor="pink" />
           <Teacher />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route path="/actuals">
           <Navigation BackgroundColor="yellow" />
           <Actuals />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route path="/actual">
           <Navigation BackgroundColor="yellow" />
           <Actual />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route path="/operation">
           <Navigation BackgroundColor="green" />
           <Operation />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route path="/enrollment">
           <Navigation BackgroundColor="blue-1" />
           <Enrollment />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route path="/contact">
           <Navigation BackgroundColor="blue-4" />
           <Contact />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route path="/courses/eurhythmics">
           <Navigation BackgroundColor="green-1" isHeaderFixed="true" />
           <Eurhythmics />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route path="/courses/callisthenics">
           <Navigation BackgroundColor="green-1" isHeaderFixed="true" />
           <Callisthenics />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route path="/courses/pre_ballet">
           <Navigation BackgroundColor="green-1" isHeaderFixed="true" />
           <PreBallet />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route path="/courses/ballet">
           <Navigation BackgroundColor="green-1" isHeaderFixed="true" />
           <Ballet />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route path="/courses/jazz">
           <Navigation BackgroundColor="green-1" isHeaderFixed="true" />
           <Jazz />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route path="/courses/modern">
           <Navigation BackgroundColor="green-1" isHeaderFixed="true" />
           <Modern />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route exact path="/programs">
           <Navigation BackgroundColor="blue-1" />
           <Programs />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route exact path="/programs/speech_day">
           <Navigation BackgroundColor="blue-1" isHeaderFixed="true" />
           <ProgramPage opt={programs.programPage.speechDay} />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route exact path="/programs/competitions">
           <Navigation BackgroundColor="blue-1" isHeaderFixed="true" />
           <ProgramPage opt={programs.programPage.competitions} />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route exact path="/programs/omisk">
           <Navigation BackgroundColor="blue-1" isHeaderFixed="true" />
           <ProgramPage opt={programs.programPage.omisk} />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route exact path="/programs/public_class">
           <Navigation BackgroundColor="blue-1" isHeaderFixed="true" />
           <ProgramPage opt={programs.programPage.publicClass} />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route exact path="/programs/callisthenics">
           <Navigation BackgroundColor="blue-1" isHeaderFixed="true" />
           <ProgramPage opt={programs.programPage.callisthenics} />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route exact path="/programs/camp">
           <Navigation BackgroundColor="blue-1" isHeaderFixed="true" />
           <ProgramPage opt={programs.programPage.camp} />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route exact path="/programs/achievements">
           <Navigation BackgroundColor="blue-1" />
           <Achievement />
           <Footer />
+          <FooterMobile />
         </Route>
         <Route path="/programDetail">
           <ProgramDetail />
---
+++ src/components/FooterMobile.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+import { ReactComponent as LogoWithTitle } from "../assets/img/icons/logo_w_title.svg";
+import { ReactComponent as Facebook } from "../assets/img/icons/facebook.svg";
+import { ReactComponent as Instagram } from "../assets/img/icons/instagram.svg";
+import { ReactComponent as Youtube } from "../assets/img/icons/youtube.svg";
+
+export default function Footer() {
+  return (
+    <div className="mobile-footer">
+      <div className="content-block">
+          <div className="content-header">
+              <LogoWithTitle className="logo"/>
+          </div>
+          <label className="text size-24 extra-bold">Az omiskról</label>
+          <label className="article size-12">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</label>
+          <label className="text size-24 extra-bold">Kapcsolat</label>
+          <label className="article size-12">
+              Felmerülő kérdésedre Mikusik Márta, iskola titkárunk válaszol<br/>
+              hétköznap 10-14 óra között hívhatod.<br/>
+              Telefon: 06-1-2000-138<br/>
+              Mobil: 06-20-9460-848<br/>
+              E-mail: iroda@omisk.hu<br/>
+              Posta: 1021. Budapest, Széher út 77.<br/>
+          </label>
+          <label className="text size-24 extra-bold">Kurzusaink</label>
+          <label className="article size-12">
+              Művészi torna<br/>
+              Mozdulatművészet<br/>
+              Modern tánc<br/>
+              Jazz tánc<br/>
+              Pre balett<br/>
+              Klasszikus balett
+          </label>
+          <label className="text size-24 extra-bold">Helyszíneink</label>
+          <label className="article size-12">
+              Óbudai Kulturális Központ<br/>
+              MOM Kulturális Központ<br/>
+              Budapest Művelődési központ<br/>
+              Vll. Kerületi Táncstúdió<br/>
+              Békásmegyeri Közösségi Ház<br/>
+              Virányosi Közösségi Ház<br/>
+              Józsefvárosi Galéria és Rendezvényközpont<br/>
+          </label>
+          <div className="sm-icons">
+              <a href="https://www.instagram.com/o_m_i_s_k/" target="_blank" rel="noopener noreferrer" className="icon">
+                  <Instagram/>
+              </a>
+              <a href="https://www.facebook.com/omisk.hu/" target="_blank" rel="noopener noreferrer" className="icon">
+                  <Facebook/>
+              </a>
+              <a href="https://www.youtube.com/channel/UCyG_fD6zAsXyDDuzShoznIA" target="_blank" rel="noopener noreferrer" className="icon">
+                  <Youtube/>
+              </a>
+          </div>
+      </div>
+    </div>
+  );
+}
