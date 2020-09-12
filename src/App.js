7728803ab6ab20da14db0a0b22e84ef25e951f02
csati <ati.csordas@gmail.com>
2020-09-13 00:20:01 +0200

    Fix conflict

--- src/App.js
+++ src/App.js
@@ -100,11 +100,7 @@ function App() {
           <FooterMobile />
         </Route>
         <Route path="/courses/eurhythmics">
-<<<<<<< HEAD
           <Navigation BackgroundColor="green-1" isHeaderFixed="true" />
-=======
-          <Navigation BackgroundColor="green" isHeaderFixed="true" />
->>>>>>> Update courses page
           <Eurhythmics />
           <Footer />
           <FooterMobile />
