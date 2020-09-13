44be39756388d359e209ab0e4ece6c6c235174ab
csati <ati.csordas@gmail.com>
2020-09-13 23:36:35 +0200

    Update programs layout

--- src/pages/ProgramPage.js
+++ src/pages/ProgramPage.js
@@ -11,27 +11,6 @@ import pagesConfig from "../assets/pagesConfig";
 
 const { actuals, ...rest } = pagesConfig;
 
-const Events = function(props) {
-  return props.list.map((event, index) => (
-    <div key={index} className={style["event-wrapper"]}>
-      <div className={style["event-header"]}>
-        <div className={style["event-date"]}>
-          <p>{event.date}</p>
-        </div>
-        <div className={style["event-details"]}>
-          <p>•••</p>
-        </div>
-      </div>
-      <div className={style["event-title"]}>
-        <h5>{event.title}</h5>
-      </div>
-      <div className={style["event-body"]}>
-        <p>{event.body}</p>
-      </div>
-    </div>
-  ));
-};
-
 const ImageGallery = function(props) {
   const images = props.list.map((img, inde) => {
     return require(`../assets/img/programs/${img}.jpg`);
@@ -42,40 +21,10 @@ const ImageGallery = function(props) {
   });
 };
 
-const ActualCard = function() {
-  // this filter black magic needed to only one card will appear in the blog post section
-  return actuals.cards
-    .filter((card, index) => index === 0)
-    .map((card, index) => (
-      <Link to={{ pathname: "/actual", state: card }} key={index}>
-        <div className="card">
-          <div className="picture-box">
-            <img
-              src={require(`../assets/img/actuals/${card.picture}`)}
-              alt=""
-            />
-            <label className="picture-title size-28 extra-bold">
-              {card.picture_title}
-            </label>
-          </div>
-          <div className="text-box">
-            <label className="actual-date size-15 extra-bold">
-              {card.date}
-            </label>
-            <label className="dot-dot-dot size-36 extra-bold">...</label>
-            <label className="actual-desc size-24 extra-bold">
-              {card.title}
-            </label>
-            <label className="actual-text size-15 thin">{card.text}</label>
-          </div>
-        </div>
-      </Link>
-    ));
-};
-
 export default function ProgramPage(props) {
   var backgroundImage = require(`../assets/img/programs/header/${props.opt.headerImg}.jpg`);
 
+  let bg = require(`../assets/img/programs/evzaroeloadas/evzaro_eloadasok_bg.jpg`);
   return (
     <div style={{ position: "relative" }}>
       <div className={style["header-wrapper"]}>
@@ -93,74 +42,35 @@ export default function ProgramPage(props) {
       <div className={style["body"]}>
         <div className={style["info"]}>
           <div className={style["details"]}>
-            <p className="size-20">{props.opt.detailsOne}</p>
-            <p className="size-20">{props.opt.detailsTwo}</p>
-            <p className="size-20">{props.opt.detailsThree || ""}</p>
-            <p className="size-20">{props.opt.detailsFour || ""}</p>
+            <p
+              className={`${style["details-text"]} ${style["first-paragraph"]} size-20`}
+            >
+              {props.opt.detailsOne}
+            </p>
+            <div className={`${style["video-wrapper"]}`}>
+              <button className={`${style["open-video"]}`}>
+                <label
+                  className={`${style["open-video-label"]} size-36 extra-bold`}
+                >
+                  Évzáró előadások
+                </label>
+                <span className="size-15 extra-bold">Videó lejátszása</span>
+              </button>
+              <img className={`${style["video-bg"]}`} src={bg} />
+            </div>
+            <p className={`${style["details-text"]} size-20`}>
+              {props.opt.detailsTwo}
+            </p>
+            <p className={`${style["details-text"]} size-20`}>
+              {props.opt.detailsThree || ""}
+            </p>
+            <p className={`${style["details-text"]} size-20`}>
+              {props.opt.detailsFour || ""}
+            </p>
           </div>
-          <div className={style["events"]}>
-            {/* <Events list={props.opt.events} /> */}
-          </div>
-        </div>
-        <div className={style["main-pic-wrapper"]}>
-          {props.opt.youtubeLink ? (
-            <iframe
-              title="video"
-              width="832"
-              height="468"
-              src={`${props.opt.youtubeLink}`}
-              frameBorder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          ) : (
-            ""
-          )}
-        </div>
-        <div className={style["alt-text"]}>
-          <p>{props.opt.videoAlt || ""}</p>
         </div>
       </div>
-      <div className={style["images"]}>
-        <div className={style["images-title"]}>
-          <h5 className="size-36">Kövess minket</h5>
-        </div>
-        <div className={style["logos"]}>
-          <a
-            href="https://www.instagram.com/o_m_i_s_k/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={style["icon"]}
-          >
-            <Instagram />
-          </a>
-          <a
-            href="https://www.facebook.com/omisk.hu/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={style["icon"]}
-          >
-            <Facebook />
-          </a>
-          <a
-            href="https://www.youtube.com/channel/UCyG_fD6zAsXyDDuzShoznIA"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={style["icon"]}
-          >
-            <Youtube />
-          </a>
-        </div>
-        <div className={style["gallery"]}>
-          <ImageGallery list={props.opt.gallery} />
-        </div>
-      </div>
-      <div className="actuals">
-        <h2 className={`${style["actual-title"]} size-48`}>Eseményeink</h2>
-        <div className="actual-cards">
-          <ActualCard />
-        </div>
-      </div>
+      <div className={style["images"]}></div>
     </div>
   );
 }
