af9cabed8c31cc0821dd56b42df1a5a12be97406
csordasattila <attila.csordas@gravityrd.com>
2020-10-29 17:00:19 +0100

    Fix typo

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,103 +1,194 @@
-import React, {useState} from "react";
-import '../assets/css/contact.css';
+import React, { useState } from "react";
+import "../assets/css/contact.css";
 
 import pagesConfig from "../assets/pagesConfig";
-import Pdf from '../assets/docs/adatkezelesi_tajekoztato.pdf';
+import Pdf from "../assets/docs/adatkezelesi_tajekoztato.pdf";
 
 import { ReactComponent as PlusSign } from "../assets/img/icons/plus.svg";
 import { ReactComponent as MinusSign } from "../assets/img/icons/minus.svg";
 
 const { contact } = pagesConfig;
 
-const Gyik = function() {
+const Gyik = function () {
   const [activeFaq, setActiveFaq] = useState(null);
 
   return contact.gyik.map((item, index) => (
-    <div className={`question ${ activeFaq === index && 'active' }`} onClick={() => setActiveFaq(activeFaq === index ? null : index) } key={index}>
-        <PlusSign className="plus-sign"/>
-        <MinusSign className="minus-sign"/>
-        <label className="question-label size-24 extra-bold">{ item.question }</label>
-        <div className="question-informations">
-          <label className="answer size-20 thin">{ item.answer }</label>
-        </div>
+    <div
+      className={`question ${activeFaq === index && "active"}`}
+      onClick={() => setActiveFaq(activeFaq === index ? null : index)}
+      key={index}
+    >
+      <PlusSign className="plus-sign" />
+      <MinusSign className="minus-sign" />
+      <label className="question-label size-24 extra-bold">
+        {item.question}
+      </label>
+      <div className="question-informations">
+        <label className="answer size-20 thin">{item.answer}</label>
+      </div>
     </div>
   ));
-}
+};
 
-const sendMail = function(mail) {
+const sendMail = function (mail) {
   const mailTo = "iroda@omisk.hu",
-        mailSubject = 'Webes kapcsolatba lépés',
-        message = "Név: " + mail.name +
-                  "\nE-mail: " + mail.email +
-                  "\nMobil: " + mail.phone +
-                  "\nMegjegyzés: " + mail.comment,
-        requestOptions = {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            to: mailTo,
-            subject: mailSubject,
-            msg: message
-          })
-        };
+    mailSubject = "Webes kapcsolatba lépés",
+    message =
+      "Név: " +
+      mail.name +
+      "\nE-mail: " +
+      mail.email +
+      "\nMobil: " +
+      mail.phone +
+      "\nMegjegyzés: " +
+      mail.comment,
+    requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        to: mailTo,
+        subject: mailSubject,
+        msg: message,
+      }),
+    };
 
-  document.querySelector('.App').scrollTop = 0;
+  document.querySelector(".App").scrollTop = 0;
 
-  fetch('/sendmail.php', requestOptions).then(() => {
+  fetch("/sendmail.php", requestOptions).then(() => {
     setMailSent();
   });
-}
+};
 
-const setMailSent = function() {
+const setMailSent = function () {
   window.sessionStorage.setItem("mail", true);
-}
+};
 
 export default function Contact() {
-  const [mail, setForm] = useState({ name: '', email: '', phone: '', comment: '', gdpr: false }),
-    updateField = e => {
+  const [mail, setForm] = useState({
+      name: "",
+      email: "",
+      phone: "",
+      comment: "",
+      gdpr: false,
+    }),
+    updateField = (e) => {
       setForm({
-      ...mail,
-      [e.target.name]: e.target.name === 'gdpr' ? e.target.checked : e.target.value
-    });
-  },
-  sessionMailSent = window.localStorage.getItem("mail"),
-  [mailSent, setMailSentState] = useState(false);
+        ...mail,
+        [e.target.name]:
+          e.target.name === "gdpr" ? e.target.checked : e.target.value,
+      });
+    },
+    sessionMailSent = window.localStorage.getItem("mail"),
+    [mailSent, setMailSentState] = useState(false);
 
   return (
     <div className="contact">
       <div className="contact-content">
         <div className="contact-columns">
-          <div className="contact-box" >
+          <div className="contact-box">
             <img src={require(`../assets/img/kapcsolat.jpg`)} alt="" />
-            <label className="size-54 extra-bold">{`${(sessionMailSent || mailSent) ? "Üzenetét elküldtük ✔" : "Lépj kapcsolatba velünk!"}`}</label>
-            <form className={`send-mail-form ${(sessionMailSent || mailSent) && 'hidden'}`} onSubmit={(event)=> {event.preventDefault();setMailSentState(true);sendMail(mail)}}>
-              <input className="form-row-input size-20" name="name" type="text" required placeholder="Név" onInput={updateField} />
-              <input className="form-row-input size-20" name="email" type="email" required placeholder="E-mail" onInput={updateField} />
-              <input className="form-row-input size-20" name="phone" type="tel" placeholder="Mobil (opcionális)" onInput={updateField} />
-              <input className="form-row-input size-20" name="comment" type="text" required placeholder="Üzenet" onInput={updateField} />
+            <label className="size-54 extra-bold">{`${
+              sessionMailSent || mailSent
+                ? "Üzenetét elküldtük ✔"
+                : "Lépj kapcsolatba velünk!"
+            }`}</label>
+            <form
+              className={`send-mail-form ${
+                (sessionMailSent || mailSent) && "hidden"
+              }`}
+              onSubmit={(event) => {
+                event.preventDefault();
+                setMailSentState(true);
+                sendMail(mail);
+              }}
+            >
+              <input
+                className="form-row-input size-20"
+                name="name"
+                type="text"
+                required
+                placeholder="Név"
+                onInput={updateField}
+              />
+              <input
+                className="form-row-input size-20"
+                name="email"
+                type="email"
+                required
+                placeholder="E-mail"
+                onInput={updateField}
+              />
+              <input
+                className="form-row-input size-20"
+                name="phone"
+                type="tel"
+                placeholder="Mobil (opcionális)"
+                onInput={updateField}
+              />
+              <input
+                className="form-row-input size-20"
+                name="comment"
+                type="text"
+                required
+                placeholder="Üzenet"
+                onInput={updateField}
+              />
               <label className="checkbox-row">
-                <input type="checkbox" required name="gdpr" onInput={updateField}/>
-                <span className="size-16 thin">Ezennel hozzájárulok, hogy a fent megadott adataimat a omisk.hu-t üzemeltető Omisk a GDPR előírásaival összhangban kezelje <a href={Pdf} className="extra-bold" target="_blank" rel="noopener noreferrer">Adatvédelmi tájékoztató</a></span>
+                <input
+                  type="checkbox"
+                  required
+                  name="gdpr"
+                  onInput={updateField}
+                />
+                <span className="size-16 thin">
+                  Ezennel hozzájárulok, hogy a fent megadott adataimat a
+                  omisk.hu-t üzemeltető Omisk a GDPR előírásaival összhangban
+                  kezelje{" "}
+                  <a
+                    href={Pdf}
+                    className="extra-bold"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Adatvédelmi tájékoztató
+                  </a>
+                </span>
               </label>
-              <button type="submit" className="btn btn-primary email-sender">Elküldöm</button>
+              <button type="submit" className="btn btn-primary email-sender">
+                Elküldöm
+              </button>
             </form>
           </div>
           <div className="contact-box">
-            <label className="content-title size-54 extra-bold">Kérdéseidre válaszolunk!</label>
+            <label className="content-title size-54 extra-bold">
+              Kérdéseidre válaszolunk!
+            </label>
             <label className="content-text size-20">
-              Felmerülő kérdésedre Mikusik Márta, iskola titkárunk válaszol hétköznap 10-14 óra között hívhatod.
-              <span className="contact-row">Telefon: <b>06-1-2000-138</b></span>
-              <span className="contact-row">Mobil: <b>06-20-9460-848</b></span>
-              <span className="contact-row">E-mail: <b><a href="mailto:iroda@omisk.hu">iroda@omisk.hu</a></b></span>
-              <span className="contact-row">Posta: <b>1021 Budapest, Széher út 77.</b></span>
+              Felmerülő kérdéseidre Mikusik Márta iskolatitkárunk válaszol.
+              Hétköznap 10-14 óra között hívhatod.
+              <span className="contact-row">
+                Telefon: <b>06-1-2000-138</b>
+              </span>
+              <span className="contact-row">
+                Mobil: <b>06-20-9460-848</b>
+              </span>
+              <span className="contact-row">
+                E-mail:{" "}
+                <b>
+                  <a href="mailto:iroda@omisk.hu">iroda@omisk.hu</a>
+                </b>
+              </span>
+              <span className="contact-row">
+                Posta: <b>1021 Budapest, Széher út 77.</b>
+              </span>
             </label>
           </div>
         </div>
       </div>
-      <div className="faq" id={'gyik'}>
+      <div className="faq" id={"gyik"}>
         <label className="size-54 extra-bold">Gyakori kérdések</label>
         <div className="drop-down-list">
-          <Gyik/>
+          <Gyik />
         </div>
       </div>
     </div>
