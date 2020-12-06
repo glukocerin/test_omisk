a0dc78ac509466b4b59c4b22637959e7e9335ead
vutengklanricsi <richard.vuongtanquang@gmail.com>
2020-12-06 15:09:43 +0100

    small fix when blog is not exist

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -10,32 +10,38 @@ import pagesConfig from "../assets/pagesConfig";
 const { blog } = pagesConfig;
 
 const BlogCards = function () {
-  return blog.cards.map((card, index) => (
-    <Link to={{ pathname: "/blog_entry", state: card }} key={index}>
-      <div className="card">
-        <div className="picture-box">
-          <img src={require(`../assets/img/blog/${card.picture}`)} alt="" />
-          <label className="picture-title size-28 extra-bold">
-            {card.picture_title}
-            <br />
-            <span className="date-for-mobile">{card.date}</span>
-          </label>
+  // console.log(blog.cards);
+  if (blog.cards) {
+    return blog.cards.map((card, index) => (
+      <Link to={{ pathname: "/blog_entry", state: card }} key={index}>
+        <div className="card">
+          <div className="picture-box">
+            <img src={require(`../assets/img/blog/${card.picture}`)} alt="" />
+            <label className="picture-title size-28 extra-bold">
+              {card.picture_title}
+              <br />
+              <span className="date-for-mobile">{card.date}</span>
+            </label>
+          </div>
+          <div className="text-box">
+            <label className="blog-entry-date size-15 extra-bold">
+              {card.date}
+            </label>
+            <label className="blog-entry-desc size-24 extra-bold">
+              {card.title}
+            </label>
+            <label className="blog-entry-text size-15 thin">
+              {/* {splitText(card.text)} */}
+            </label>
+          </div>
         </div>
-        <div className="text-box">
-          <label className="blog-entry-date size-15 extra-bold">
-            {card.date}
-          </label>
-          <label className="blog-entry-desc size-24 extra-bold">
-            {card.title}
-          </label>
-          <label className="blog-entry-text size-15 thin">
-            {/* {splitText(card.text)} */}
-            
-          </label>
-        </div>
-      </div>
-    </Link>
-  ));
+      </Link>
+    ));
+  } else {
+    return (
+      <p className="title size-90 extra-bold">A Tartalom még nem elérhető!</p>
+    );
+  }
 };
 
 function splitText(text) {
