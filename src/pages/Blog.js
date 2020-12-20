83061af207c055bb812e524865f2f7263e652ca3
csati <ati.csordas@gmail.com>
2020-12-21 00:58:44 +0100

    Add new blog entries

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -10,31 +10,37 @@ import pagesConfig from "../assets/pagesConfig";
 const { blog } = pagesConfig;
 
 const BlogCards = function () {
-  return blog.cards.map((card, index) => (
-    <Link to={{ pathname: "/blog_entry", state: card }} key={index}>
-      <div className="card">
-        <div className="picture-box">
-          <img src={require(`../assets/img/blog/${card.picture}`)} alt="" />
-          <label className="picture-title size-28 extra-bold">
-            {card.picture_title}
-            <br />
-            <span className="date-for-mobile">{card.date}</span>
-          </label>
+  return blog.cards.map((card, index) => {
+    const image = card["picture_card"]
+      ? require(`../assets/img/blog/${card.picture_card}`)
+      : require(`../assets/img/blog/${card.picture}`);
+
+    return (
+      <Link to={{ pathname: "/blog_entry", state: card }} key={index}>
+        <div className="card">
+          <div className="picture-box">
+            <img src={image} alt="" />
+            <label className="picture-title size-28 extra-bold">
+              {card.picture_title}
+              <br />
+              <span className="date-for-mobile">{card.date}</span>
+            </label>
+          </div>
+          <div className="text-box">
+            <label className="blog-entry-date size-15 extra-bold">
+              {card.date}
+            </label>
+            <label className="blog-entry-desc size-24 extra-bold">
+              {card.title}
+            </label>
+            <label className="blog-entry-text size-15 thin">
+              {splitText(card.textFirst)}
+            </label>
+          </div>
         </div>
-        <div className="text-box">
-          <label className="blog-entry-date size-15 extra-bold">
-            {card.date}
-          </label>
-          <label className="blog-entry-desc size-24 extra-bold">
-            {card.title}
-          </label>
-          <label className="blog-entry-text size-15 thin">
-            {splitText(card.text)}
-          </label>
-        </div>
-      </div>
-    </Link>
-  ));
+      </Link>
+    );
+  });
 };
 
 function splitText(text) {
--- src/pages/BlogEntry.js
+++ src/pages/BlogEntry.js
@@ -1,5 +1,5 @@
 import React from "react";
-import '../assets/css/blog.entry.css';
+import "../assets/css/blog.entry.css";
 
 import { Link, useLocation } from "react-router-dom";
 
@@ -12,16 +12,23 @@ export default function BlogEntry() {
       <label className="state-location size-13">
         <Link to="/blog">Vissza a blogra</Link>
       </label>
-      <label className="title size-90 extra-bold">{ blogEntry.title }</label>
+      <label className="title size-90 extra-bold">{blogEntry.title}</label>
       <div className="blog-entry-box">
-        <label className="date size-15 bold">{ blogEntry.date }</label>
-        <label className="details size-20 thin" dangerouslySetInnerHTML={createMarkup(blogEntry.text)}></label>
+        <label className="date size-15 bold">{blogEntry.date}</label>
+        <label
+          className="details-first details size-20 thin"
+          dangerouslySetInnerHTML={createMarkup(blogEntry.textFirst)}
+        ></label>
+        <img src={require(`../assets/img/blog/${blogEntry.picture}`)} alt="" />
+        <label
+          className="details size-20 thin"
+          dangerouslySetInnerHTML={createMarkup(blogEntry.textSecond)}
+        ></label>
       </div>
-      <img src={require(`../assets/img/blog/${blogEntry.picture}`)} alt="" />
     </div>
   );
 }
 
 function createMarkup(markup) {
-  return {__html: markup};
+  return { __html: markup };
 }
