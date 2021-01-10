ac8f96e820d04d7ca87aadaba8b9c14b9ff1466f
csati <ati.csordas@gmail.com>
2021-01-10 20:14:46 +0100

    Fix covid and cookies notifications

--- src/components/CovidNotification.js
+++ src/components/CovidNotification.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../assets/css/covidNotification.css";
 import pagesConfig from "../assets/pagesConfig";
+import { ReactComponent as CovidIcon } from "../assets/img/icons/covid.svg";
 
 export default function CovidNotification() {
   const [closed, closeCookies] = useState(false);
@@ -16,7 +17,8 @@ export default function CovidNotification() {
         <button className="btn close" onClick={() => closeCookies(true)}>
           &#215;
         </button>
-        <label className="size-20 thin">
+        <label className="size-20 thin covid-label">
+          <CovidIcon className="icon" />
           Jelenleg online folytatjuk kurzusainkat,{" "}
           <Link
             onClick={() => {
@@ -33,9 +35,6 @@ export default function CovidNotification() {
         <Link
           to={{ pathname: "/blog_entry", state: covidBlogEntry }}
           className="btn accept"
-          onClick={() => {
-            closeCookies(true);
-          }}
         >
           Bővebben
         </Link>
