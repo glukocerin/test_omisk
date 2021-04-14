5b981aa160d38e9f5a44108b2e50fd269b4bbdc9
TAJ <bestrapboy@gmail.com>
2021-04-14 22:27:41 +0200

    add first part of camp mobile

--- src/pages/CampPageMobile.js
+++ src/pages/CampPageMobile.js
@@ -26,23 +26,22 @@ export default function CampPageMobile({ props }) {
       <div className={style["body"]}>
         <div className={style["info"]}>
           <div className={style["camp-row"]}>
-            <img className={style["camp-row-image"]} src={require(`../assets/img/programs/camp/camp01.jpg`).default} alt=""/>
             <div className={style["camp-row-details"]}>
-              <label className="size-54 bold">{ props.opt.content.first_block.title }</label>
+              <label className="size-32 bold">{ props.opt.content.first_block.title }</label>
               <label className="size-24" dangerouslySetInnerHTML={{ __html: props.opt.content.first_block.place }}></label>
-              <label className="size-20">{ props.opt.content.first_block.programs }</label>
-              <label className="size-20" dangerouslySetInnerHTML={{ __html: props.opt.content.first_block.price }}></label>
-              <label className="size-20" dangerouslySetInnerHTML={{ __html: props.opt.content.first_block.block_desc }}></label>
-              {/* TODO: add link */}
-              <button className={`${style['btn']} ${style['primary']}`}>Jelentkezem</button>
+              <label className="size-32 bold">{ props.opt.content.second_block.title }</label>
+              <label className="size-24" dangerouslySetInnerHTML={{ __html: props.opt.content.first_block.place }}></label>
             </div>
+            <img className={style["camp-row-image"]} src={require(`../assets/img/programs/camp/camp01.jpg`).default} alt=""/>
           </div>
           <div className={style["camp-row"]}>
             <div className={style["camp-row-details"]}>
-              <label className="size-54 bold">{ props.opt.content.second_block.title }</label>
-              <label className="size-24" dangerouslySetInnerHTML={{ __html: props.opt.content.first_block.place }}></label>
-              <label className="size-20">{ props.opt.content.second_block.programs }</label>
-              <label className="size-20" dangerouslySetInnerHTML={{ __html: props.opt.content.second_block.price }}></label>
+              <label className="size-20"><strong>Korosztályok: <span className={style['blue']}>6-12 és 13-17</span></strong></label>
+              <label className="size-20">
+                <span className="bold">Részvételi díj: 42 000Ft</span><br/>
+                <span>Iskolánk növendékeinek</span><br/>
+                <span>kedvezményesen: <strong>39.000 Ft</strong></span>
+              </label>
               {/* TODO: add link */}
               <button className={`${style['btn']} ${style['primary']}`}>Jelentkezem</button>
             </div>
@@ -55,24 +54,26 @@ export default function CampPageMobile({ props }) {
               {props.opt.detailsOne}
             </p>
           </div>
-          <div className={`${style["camp-row"]} ${style["cover"]} ${style["bg03"]}`}>
+          <div className={style["camp-row"]}>
+            <img className={style["camp-row-image"]} src={require(`../assets/img/programs/camp/camp03cover.jpg`).default} alt=""/>
             <div className={style["apply-box"]}>
               <div className={style["block"]}>
-                <label className="size-54 bold">Jelentkezz most!</label>
-                <label className="size-36 bold">4 féle táncstílus</label>
-                <label className="size-36 bold">Tánc és gimnasztika</label>
-                <label className="size-36 bold">Tánctermi és szabadtéri programok</label>
-                <label className="size-36 bold">Táborzáró előadás</label>
+                <label className="size-32 bold">Jelentkezz most!</label>
+                <label className="size-20 bold">4 féle táncstílus</label>
+                <label className="size-20 bold">Tánc és gimnasztika</label>
+                <label className="size-20 bold">Tánctermi és szabadtéri programok</label>
+                <label className="size-20 bold">Táborzáró előadás</label>
               </div>
               <div className={style["block"]}>
-                <label className="size-54 bold">Kapcsolat</label>
-                <label className="size-36 bold">Solti Eszter</label>
-                <label className="size-36 bold">omisktabor@gmail.com</label>
-                <label className="size-36 bold">06-30-237-9233</label>
+                <label className="size-32 bold">Kapcsolat</label>
+                <label className="size-20 bold">Solti Eszter</label>
+                <label className="size-20 bold">omisktabor@gmail.com</label>
+                <label className="size-20 bold">06-30-237-9233</label>
               </div>
             </div>
           </div>
-          <div className={`${style["camp-row"]} ${style["cover"]} ${style["bg04"]}`}>
+          <div className={style["camp-row"]}>
+            <img className={style["camp-row-image"]} src={require(`../assets/img/programs/camp/camp04cover.jpg`).default} alt=""/>
             <label className="size-54 bold">Programjaink</label>
             {/* TODO: add links */}
             <div className={style['button-box']}>
@@ -90,7 +91,7 @@ export default function CampPageMobile({ props }) {
             </div>
             <button className={`${style['btn']} ${style['secondary']}`}>Részletesen a  programokról</button>
           </div>
-          <div className={`${style["camp-row"]} ${style["cover"]} ${style["bg05"]}`}>
+          <div className={style["camp-row"]}>
             <div className={style["day-goes-by-box"]}>
               <label className="size-36 bold">Hogyan telik egy nap</label>
               <label className="size-20">
@@ -101,35 +102,14 @@ export default function CampPageMobile({ props }) {
           </div>
           <div className={style["camp-row"]}>
             <img className={style["camp-row-image"]} src={require(`../assets/img/programs/camp/camp06.jpg`).default} alt=""/>
-            <img className={style["camp-row-image"]} src={require(`../assets/img/programs/camp/camp07.jpg`).default} alt=""/>
           </div>
-          <div className={`${style["camp-row"]} ${style["cover"]} ${style["bg08"]}`}>
+          <div className={style["camp-row"]}>
             <div className={style["daycare-box"]}>
               <label className="size-36 bold">Jelentkezés</label>
               <label className="size-20">
                 A napközis táborokba az alábbi linken található <strong>jelentkezési lap online kitöltésével lehet:</strong>
               </label>
               <Link to={'/programs/camp'} className={`${style['apply-link']} size-20 bold`}>Jelentkezési lap</Link>
-              <button className={`${style['btn']} ${style['secondary']}`}>Tovább olvasom</button>
-            </div>
-          </div>
-          <div className={style["camp-row"]}>
-            <img className={style["camp-row-image"]} src={require(`../assets/img/programs/camp/camp09.jpg`).default} alt=""/>
-            <img className={style["camp-row-image"]} src={require(`../assets/img/programs/camp/camp10.jpg`).default} alt=""/>
-          </div>
-          <div className={style["camp-row"]}>
-            <img className={style["camp-row-image"]} src={require(`../assets/img/programs/camp/camp12.jpg`).default} alt=""/>
-            <img className={style["camp-row-image"]} src={require(`../assets/img/programs/camp/camp13.jpg`).default} alt=""/>
-          </div>
-          <div className={`${style["camp-row"]} ${style["cover"]} ${style["bg14"]}`}>
-            <div className={style["faq-box"]}>
-              <label className="size-36 bold">Gyakori kérdések</label>
-              <label className="size-20 bold">
-                Mikor kezdődik a tábori nap és meddig tart?
-              </label>
-              <label className="size-20">
-                A táborba minden nap reggel 8 órától lehet érkezni, a programok  9 órakor kezdődnek. 8 órától biztosítjuk a tanMikor kezdődik a tábori nap és meddig tart?ári felügyeletet, így akinek reggel sietősebb, hozhatja kezdésre gyermekét, de akinek kényelmesebb, lehet csak a programok kezdetére érkezni.<br/>...
-              </label>
               <button className={`${style['btn']} ${style['secondary']}`}>Tovább olvasom</button>
             </div>
           </div>
